f48c3832b726cb42cf7461f59316144cdb3c02fe
Miguel Diaz <miguel.diaz@codescrum.com>
2017-02-27 19:33:40 -0500

    modified config/initializers/session_store.rb to ask for a SESSION_STORE_KEY_NAME (automatically appends environment)

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,11 @@
 # Be sure to restart your server when you modify this file.
+session_store_key_name = ENV['SESSION_STORE_KEY_NAME']
 
-Rails.application.config.session_store :cookie_store, key: '_rails5-template_session'
+valid_session_store_key_name = session_store_key_name.is_a?(String) && session_store_key_name.present?
+
+raise "Please define a cookie_store key at `config/initializers/session_store.rb` (or in your .env file under SESSION_STORE_KEY_NAME)" unless valid_session_store_key_name
+
+# Use a different cookie for each environment
+session_store_key = "_#{session_store_key_name}-#{Rails.env}"
+
+Rails.application.config.session_store :cookie_store, key: session_store_key
